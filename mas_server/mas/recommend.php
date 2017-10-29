4341176ef2c483f4ac1b4c8de0a2f96672cc41a1
jhh1117 <winery11234@gmail.com>
2017-10-29 13:14:16 +0900

    Update recommend.php

--- mas_server/mas/recommend.php
+++ mas_server/mas/recommend.php
@@ -1,18 +1,6 @@
 <?php
 
-/**
- * PHP item based filtering
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * @package   PHP item based filtering
- */
-
 class Recommend {
-
     
     public function similarityDistance($preferences, $person1, $person2)
     {
